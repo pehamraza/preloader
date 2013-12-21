c6efbf432e9ac2b343ea62010cf23ccaab5be4a6
Peham Raza <m@pehamraza.com>
2013-12-22 00:02:40 +0500

    very first commit for the project

---
+++ js.js
@@ -0,0 +1,10 @@
+/*************************************
+CSS written by Peham Raza
+on 20 December 2013
+**********************************/
+
+function show_page()
+{
+	$('#loading').hide();
+	$('#content').removeClass('hideit');
+}
